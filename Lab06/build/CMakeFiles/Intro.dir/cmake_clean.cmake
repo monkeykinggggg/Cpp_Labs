1e81da3468a8e500c7fc2742421d29809e1a1efc
Joanna <joanna.heldak@gmail.com>
2024-07-04 19:08:12 +0200

    learning classes

---
+++ Lab06/build/CMakeFiles/Intro.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Intro.dir/Main.cpp.o"
+  "CMakeFiles/Intro.dir/Main.cpp.o.d"
+  "CMakeFiles/Intro.dir/src/Angle.cpp.o"
+  "CMakeFiles/Intro.dir/src/Angle.cpp.o.d"
+  "Intro"
+  "Intro.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Intro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
